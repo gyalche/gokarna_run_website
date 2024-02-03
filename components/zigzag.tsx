5c77bf869444ecb434f4d703aa4deea8cf35438a
dawa sherpa <sherpadawag1998@gmail.com>
2024-02-03 14:53:30 +0545

    gokarna website

---
+++ components/zigzag.tsx
@@ -0,0 +1,229 @@
+import Image from 'next/image';
+
+import FeatImage01 from '@/public/images/features-03-image-01.png';
+import FeatImage02 from '@/public/images/features-03-image-02.png';
+import FeatImage03 from '@/public/images/features-03-image-03.png';
+
+export default function Zigzag() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="py-12 md:py-20 border-t border-gray-800">
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">
+              Reach goals that matter
+            </div>
+            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+            <p className="text-xl text-gray-400">
+              Excepteur sint occaecat cupidata
+            </p>
+          </div>
+
+          {/* Items */}
+          <div className="grid gap-20">
+            {/* 1st item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up">
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src="/images/g5.jpg"
+                  width={540}
+                  height={405}
+                  alt="Features 01"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    More speed. Less spend
+                  </div>
+                  <h3 className="h3 mb-3">Keep run on schedule</h3>
+                  {/* <p className="text-xl text-gray-400 mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                  </p> */}
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      {/* <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg> */}
+                      <span>
+                        1st week of Gokarneshowr Run successfully completed
+                        mayor of Gokarneshowr Municipality Deepak Risal was
+                        Chief Guest and Gokarneshowr Municipality Co-ordinator
+                        Of Sports Mr Sanjay Lama and Rajan karki (president
+                        Athletic pariwar) was special Guest, Thank you for all!
+                        Mira RaiGorkhali Tenji Nitu Tamang Jaya Raja dai, Nabin
+                        K Sunuwar @Lakpa Lama Kritim Shrestha @Tek Bahadur Rai
+                        @Rohan Rai @Viswa Hang Mira Rai InitiativeExchange And
+                        Empower
+                      </span>
+                    </li>
+                    {/* <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Excepteur sint occaecat</span>
+                    </li> */}
+                    {/* <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Amet consectetur adipiscing elit</span>
+                    </li> */}
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 2nd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl"
+                data-aos="fade-up">
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src="/images/g7.jpg"
+                  width={540}
+                  height={405}
+                  alt="Features 02"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-left">
+                <div className="md:pl-4 lg:pl-12 xl:pl-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    I run because i can.
+                  </div>
+                  <h3 className="h3 mb-3">Nepal Run UK Shoes Campaign</h3>
+                  <p className="text-xl text-gray-400 mb-4">
+                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. */}
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Duis aute irure dolor in reprehenderit</span>
+                    </li>
+                    <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Excepteur sint occaecat</span>
+                    </li>
+                    <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Amet consectetur adipiscing elit</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            {/* 3rd item */}
+            <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
+              {/* Image */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1"
+                data-aos="fade-up">
+                <Image
+                  className="max-w-full mx-auto md:max-w-none h-auto"
+                  src="/images/g1.jpg"
+                  width={540}
+                  height={305}
+                  objectFit="contain"
+                  alt="Features 03"
+                />
+              </div>
+              {/* Content */}
+              <div
+                className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6"
+                data-aos="fade-right">
+                <div className="md:pr-4 lg:pr-12 xl:pr-16">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">
+                    About us.
+                  </div>
+                  {/* <h3 className="h3 mb-3">Keep projects on schedule</h3> */}
+                  <p className="text-xl text-gray-400 mb-4">
+                    Gorkhali Tenji Gokarneshwor Run Samuha, established by
+                    Mr.Tenji Sherpa on January 06, 2024, is a non-profit
+                    organization hosting weekly running events inspired by the
+                    UK's Park Run and Dharan Run's Santosh Rai. Every Saturday
+                    at 7:00 am, the free timed run starts from Sahid Park Gate,
+                    covering a 3.7 km route over two rounds. Participants range
+                    from 4-year-old juniors to 70-year-old seniors, and there
+                    are no strict rules – you can run, jog, or walk, wearing
+                    whatever feels comfortable.
+                  </p>
+                  <ul className="text-lg text-gray-400 -mb-2">
+                    {/* <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Duis aute irure dolor in reprehenderit</span>
+                    </li> */}
+                    {/* <li className="flex items-center mb-2">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Excepteur sint occaecat</span>
+                    </li> */}
+                    {/* <li className="flex items-center">
+                      <svg
+                        className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                      </svg>
+                      <span>Amet consectetur adipiscing elit</span>
+                    </li> */}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
